8f9c9d388488ede57eba129b56a5013a70751c7a
Otsu01 <kudo.nonoka02@gmail.com>
2023-04-27 04:44:57 +0900

    [add]Start時に石を中央に４つ設置

--- Assets/Scripts/BoardGenerateController.cs
+++ Assets/Scripts/BoardGenerateController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class BoardGenerateController : MonoBehaviour
 {
     [SerializeField, Tooltip("1マス分のBoardPrefab")] GameObject _OneCellBoardPrefab;
+    [SerializeField, Tooltip("最初のDisc設置のためのPrefab")] GameObject[] _discPrefab;
     float _OneCellBoardHeight = 1;
     float _instatePosX;
     float _instatePosZ;
@@ -32,9 +33,24 @@ public class BoardGenerateController : MonoBehaviour
                     Debug.Log("１マス用のPrefabObjectにCellControllerをつけてください");
                 }
                 _instatePosX += _OneCellBoardHeight;
+
+                //最初となる石を設置
+                StartDiscON(i, j, oneCell.transform.position);
             }
             _instatePosX = (-_OneCellBoardHeight * 4) + (_OneCellBoardHeight / 2);
             _instatePosZ -= _OneCellBoardHeight;
+        }
+    }
+
+    void StartDiscON(int line, char row, Vector3 pos)
+    {
+        if((line == 4  && row == 'd') || (line == 5 && row == 'e'))
+        {
+            Instantiate(_discPrefab[1], pos, Quaternion.identity);
+        }
+        else if ((line == 4 && row == 'e') || (line == 5 && row == 'd'))
+        {
+            Instantiate(_discPrefab[0], pos, Quaternion.identity);
         }
     }
 }
