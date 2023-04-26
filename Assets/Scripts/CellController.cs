c959325531d64eba779f290d830af7b20ceb44ea
Otsu01 <kudo.nonoka02@gmail.com>
2023-04-27 04:27:33 +0900

    [updata]マスの上に石があるかどうか判定できるようにした

--- Assets/Scripts/CellController.cs
+++ Assets/Scripts/CellController.cs
@@ -6,4 +6,13 @@ public class CellController : MonoBehaviour
 {
     [SerializeField]string _myCellNum = "";
     public string MyCellNum {get { return _myCellNum;}　set { _myCellNum = value; } }
+    /// <summary>自分のマスの上に石が置かれているかどうか</summary>
+    [SerializeField]bool _isCisc = false;
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        _isCisc = true;
+        gameObject.layer = 2;
+        transform.GetChild(0).gameObject.SetActive(false);
+    }
 }
